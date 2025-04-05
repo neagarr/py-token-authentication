21349c1695d64e08642169b18112de1cfb9e1b31
Mykola Kokoiev <veokoknik@gmail.com>
2025-04-05 21:56:47 +0200

    implement user registration api

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,8 @@
-# write your code here
+from django.urls import path
+from user.views import CreateUserView
+
+app_name = "user"
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+]
--- user/views.py
+++ user/views.py
@@ -1,1 +1,6 @@
-# write your code here
+from rest_framework import generics
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer

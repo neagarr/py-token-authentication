0a4f0f2365b5f56a45f6542d1cc054bc2e2c0dc4
Mykola Kokoiev <veokoknik@gmail.com>
2025-04-05 22:20:27 +0200

    implement token creation

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from user.views import CreateUserView
+
+from user.views import CreateUserView, CreateTokenView
 
 app_name = "user"
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", CreateTokenView.as_view(), name="create_token"),
 ]
